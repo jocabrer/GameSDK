e49c1fd3c7af67d5db1716fa01fe0a9ec7e65737
Jose Cabrera Gatica <jose.cabrera@miro.cl>
2021-12-02 08:48:17 -0300

    Se agregan paredes de concreto y se reordenan menú

---
+++ Scripts/Spawners/ism_mods/concrete_guide.lua
@@ -0,0 +1,20 @@
+
+
+local newItem = {
+  class = "guide_concrete", 
+  percent = 3, 
+}
+
+local categoryToAdjust = FindInTable(ItemSpawnerManager.itemCategories, "category", "RandomCraftingGuide")
+local categoryItemToAdjust1 = FindInTable(categoryToAdjust.classes, "class", "guide_medical_bandages") 
+
+table.insert(categoryToAdjust.classes, newItem)
+
+categoryItemToAdjust1.percent = categoryItemToAdjust1.percent - newItem.percent
+
+
+
+
+
+
+
