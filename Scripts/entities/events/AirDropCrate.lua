24cbafc22eff24053403abc40816c628615e99d8
Jose Cabrera Gatica <jose.cabrera@miro.cl>
2022-01-05 14:43:41 -0300

    Ajustes drop

--- Scripts/entities/events/AirDropCrate.lua
+++ Scripts/entities/events/AirDropCrate.lua
@@ -13,6 +13,18 @@ CreateItemTable("AirDropCrate");
 
 -- Called when the crate is about to land - about 2.5m above the ground
 -- This is when the lading particle effects and audio are played
---function AirDropCrate:OnLanded()
-	--Log("AirDropCrate - OnLanded")
---end
+function AirDropCrate:OnLanded()
+        Log("AirDropCrate - OnLanded")
+        Script.SetTimerForFunction(1000, "SpawnAISingle", self)
+end
+
+SpawnAISingle = function(self)
+
+	local vForwardOffset = {x=0,y=0,z=0}
+	FastScaleVector(vForwardOffset, self:GetDirectionVector(), 0)
+	local vSpawnPos = {x=0,y=0,z=0}
+	FastSumVectors(vSpawnPos, vForwardOffset, self:GetWorldPos())
+
+	AISM.SpawnCategory(vSpawnPos, "HumanZombieSingle", true, 2.0, 5.0, 2.0)
+
+end
