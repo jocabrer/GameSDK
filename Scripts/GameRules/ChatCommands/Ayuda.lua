67f0f62923722a2fecfb2fad036921e57ee5c613
Jose Cabrera G <jocabrer@gmail.com>
2022-04-10 21:19:36 -0400

    Se agrega comando zona y se corrige id del sign cargador

--- Scripts/GameRules/ChatCommands/Ayuda.lua
+++ Scripts/GameRules/ChatCommands/Ayuda.lua
@@ -1,7 +1,31 @@
---ChatCommands["!help"] = function(playerId, command)
---	Log(">> !help - %s", command);
+ChatCommands["!help"] = function(playerId, command)
+	Log(">> !help - %s", command);
 	
-	--g_gameRules.game:SendTextMessage(4, playerId, "You can use the following commands for help / information: !rules !discord !about !admininfo !kiosk !storage !vehicles !basebuilding");
+	g_gameRules.game:SendTextMessage(4, playerId, "You can use the following commands for help / information: !rules, the command !zone will teleport you to the safe zone, the !say command allows you to send global messages");
 	
---end
+end
+
+ChatCommands["!rules"] = function(playerId, command)
+	Log(">> !rules - %s", command);
+	
+	g_gameRules.game:SendTextMessage(4, playerId, "Forbidden to steal, do not kill other players, do not be rude, do not raid bases, do not run over, we check the logs to see these misconduct");
+	
+end
+
+
+
+ChatCommands["!ayuda"] = function(playerId, command)
+	Log(">> !ayuda - %s", command);
+	
+	g_gameRules.game:SendTextMessage(4, playerId, "Puede usar los siguientes comandos para obtener ayuda/información: !reglas, el comando !zona te teletransportará hasta la zona segura, el comando !say te permite enviar mensajes globales");
+	
+end
+
+ChatCommands["!reglas"] = function(playerId, command)
+	Log(">> !reglas - %s", command);
+	
+	g_gameRules.game:SendTextMessage(4, playerId, "Prohibido robar, no matar a otros jugadores, no seas grosero, no incursionar en bases, no atropellar, nosotros revisamos los logs para ver si se incumplen las reglas");
+	
+end
+
 
--- Scripts/GameRules/ChatCommands/global.lua
+++ Scripts/GameRules/ChatCommands/global.lua
@@ -7,3 +7,16 @@ ChatCommands["!say"] = function(playerId, command)
 
 end
 
+-- Teleport to safe zone 4850 4787 
+ChatCommands['!zona'] = function(playerId, command)
+    Log('>> !zona - %s', command);
+    local player = System.GetEntity(playerId);
+    player.player:TeleportTo('4912 4718 142.8');
+end
+
+-- Teleport to safe zone 4850 4787 
+ChatCommands['!zone'] = function(playerId, command)
+    Log('>> !zone - %s', command);
+    local player = System.GetEntity(playerId);
+    player.player:TeleportTo('4912 4718 142.8');
+end
